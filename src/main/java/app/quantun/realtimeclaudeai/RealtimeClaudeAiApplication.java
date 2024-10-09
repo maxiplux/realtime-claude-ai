a3afcdab1aff179fc7c19f626a51f63aff032cf7
Juan Francisco Mosquera <maxiplux@gmail.com>
2024-10-08 20:43:16 -0400

    Initial commit

---
+++ src/main/java/app/quantun/realtimeclaudeai/RealtimeClaudeAiApplication.java
@@ -0,0 +1,13 @@
+package app.quantun.realtimeclaudeai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeClaudeAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimeClaudeAiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/app/quantun/realtimeclaudeai/controllers/ChatController.java
@@ -0,0 +1,4 @@
+package app.quantun.realtimeclaudeai.controllers;
+
+public class ChatController {
+}
